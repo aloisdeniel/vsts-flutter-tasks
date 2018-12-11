b4b0f8597554d5577bc741eed85cbeb06574fceb
Aloïs Deniel <alois.deniel@gmail.com>
2018-12-11 11:00:30 +0100

    Updated with stable versions.

--- publish.sh
+++ publish.sh
@@ -1,2 +1,18 @@
+# Build TS
+npm install -g typescript
+cd ./tasks/test
+npm install
+tsc 
+cd ../install
+npm install
+tsc
+cd ../build
+npm install
+tsc 
+cd ../../
+
+# Create extension
 npm i -g tfx-cli
-tfx extension create --manifest-globs vsts-extension.json
+tfx extension create --manifest-globs vss-extension.json
+
+# > Upload from https://marketplace.visualstudio.com/manage/publishers/aloisdeniel
