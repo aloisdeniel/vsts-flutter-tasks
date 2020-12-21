2e4c0404103b469bfefcb0c04938d7388d0f2e31
Rajat Anantharam <rajat@funda.nl>
2020-12-21 17:37:50 +0100

    Added support for AAR

--- tasks/build/index.js
+++ tasks/build/index.js
@@ -88,6 +88,33 @@ function buildApk(flutter, targetPlatform, buildName, buildNumber, debugMode, bu
         }
     });
 }
+function buildAab(flutter, buildName, buildNumber, debugMode, buildFlavour, entryPoint) {
+    return __awaiter(this, void 0, void 0, function* () {
+        var args = [
+            "build",
+            "appbundle"
+        ];
+        if (debugMode) {
+            args.push("--debug");
+        }
+        if (buildName) {
+            args.push("--build-name=" + buildName);
+        }
+        if (buildNumber) {
+            args.push("--build-number=" + buildNumber);
+        }
+        if (buildFlavour) {
+            args.push("--flavor=" + buildFlavour);
+        }
+        if (entryPoint) {
+            args.push("--target=" + entryPoint);
+        }
+        var result = yield task.exec(flutter, args);
+        if (result !== 0) {
+            throw new Error("aab build failed");
+        }
+    });
+}
 function buildAar(flutter, targetPlatform, buildName, buildNumber, debugMode, buildFlavour, entryPoint) {
     return __awaiter(this, void 0, void 0, function* () {
         var args = [
@@ -115,33 +142,6 @@ function buildAar(flutter, targetPlatform, buildName, buildNumber, debugMode, bu
         var result = yield task.exec(flutter, args);
         if (result !== 0) {
             throw new Error("aar build failed");
-        }
-    });
-}
-function buildAab(flutter, buildName, buildNumber, debugMode, buildFlavour, entryPoint) {
-    return __awaiter(this, void 0, void 0, function* () {
-        var args = [
-            "build",
-            "appbundle"
-        ];
-        if (debugMode) {
-            args.push("--debug");
-        }
-        if (buildName) {
-            args.push("--build-name=" + buildName);
-        }
-        if (buildNumber) {
-            args.push("--build-number=" + buildNumber);
-        }
-        if (buildFlavour) {
-            args.push("--flavor=" + buildFlavour);
-        }
-        if (entryPoint) {
-            args.push("--target=" + entryPoint);
-        }
-        var result = yield task.exec(flutter, args);
-        if (result !== 0) {
-            throw new Error("aab build failed");
         }
     });
 }
