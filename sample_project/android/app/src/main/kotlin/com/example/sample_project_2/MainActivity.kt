673b69f5ee8252f61917c235772c66ad6d574c24
faradayfan <faradayfan@protonmail.com>
2022-10-06 12:38:00 -0600

    Upgrading sample project

---
+++ sample_project/android/app/src/main/kotlin/com/example/sample_project_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sample_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- sample_project/test/widget_test.dart
+++ sample_project/test/widget_test.dart
@@ -1,8 +1,9 @@
 // This is a basic Flutter widget test.
-// To perform an interaction with a widget in your test, use the WidgetTester utility that Flutter
-// provides. For example, you can send tap and scroll gestures. You can also use WidgetTester to
-// find child widgets in the widget tree, read text, and verify that the values of widget properties
-// are correct.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -12,7 +13,7 @@ import 'package:sample_project/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
@@ -25,11 +26,5 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
-  });
-  testWidgets('Other test', (WidgetTester tester) async {
-
-  });
-  testWidgets('And another test', (WidgetTester tester) async {
-    //throw Error();
   });
 }
